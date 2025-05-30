01889523080b9f8611eb15e26bc3f6611ac14220
Anders Lehn Reed <anders.lehn.reed@gmail.com>
2025-05-30 22:30:01 +0200

    __main__.py for package

---
+++ ehf_relay/__main__.py
@@ -0,0 +1,5 @@
+from ehf_relay import run
+from ehf_relay.config import SOURCE, TARGET
+
+
+run(SOURCE, TARGET)
--- ehf_relay/config.py
+++ ehf_relay/config.py
@@ -1,1 +1,6 @@
+from ehf_relay.fetch.unit4 import Unit4Fetcher
+
+
 REQUEST_TIMEOUT = 20
+SOURCE = Unit4Fetcher()
+TARGET = None
